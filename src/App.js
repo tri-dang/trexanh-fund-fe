cbf127689e84027b788c41493745d099db5efcfd
Tri Dang <dangtoantri@gmail.com>
2022-06-30 17:18:11 +0700

    Add poster and use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.png';
+import poster from './poster.jpg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Quỹ Tre Xanh xin chào!</h1>
-      </header>
+    <div className="App container">
+      <header className="App-header"></header>
+      <img src={poster} className="col-lg-10 col-md-12 col-sm-12 col-xs-12 App-logo" alt="logo" />
     </div>
   );
 }
