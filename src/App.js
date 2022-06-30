ac54e3c843451231d2abe60e9fa48130443649bc
Tri Dang <dangtoantri@gmail.com>
2022-06-30 17:36:47 +0700

    Add offset for main app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App container">
       <header className="App-header"></header>
-      <div className="col-lg-10 col-md-12 col-sm-12 col-xs-12">
+      <div className="col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-xs-12">
         <img src={poster} className=" App-logo" alt="logo" />
       </div>
     </div>
